c95654196ebcf67fee29dbeb36b84fa6c6692fce
vikaspogu <vikaspoguadf@gmail.com>
2020-10-15 09:37:23 -0500

    create folder for each day

--- main.go
+++ main.go
@@ -13,12 +13,16 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var client mqtt.Client
+var (
+	client mqtt.Client
+	path   string
+)
 
 func main() {
 	godotenv.Load()
 	mqttBroker := os.Getenv("MQTT_BROKER")
 	topic := os.Getenv("TOPIC")
+	path = "/data/"
 
 	opts := mqtt.NewClientOptions().AddBroker("tcp://" + mqttBroker + ":1883").SetClientID("frigate_client")
 	client = mqtt.NewClient(opts)
@@ -45,7 +49,19 @@ func listen(topic string) {
 			log.Fatalln(err)
 		}
 		currentTime := time.Now()
-		f, err := os.Create("/data/" + currentTime.Format("2006.01.02 15:04:05") + ".jpg")
+		currentDate := currentTime.Format("2006-01-02")
+		currDir := path + currentDate
+		imageName := currentTime.Format("2006-01-02 15:04:05") + ".jpg"
+
+		if _, err := os.Stat(currDir); os.IsNotExist(err) {
+			// path/to/whatever does not exist
+			err := os.Mkdir(currDir, 0755)
+			if err != nil {
+				log.Errorf("Cannot create folder %s", currentDate)
+			}
+		}
+
+		f, err := os.Create(currDir + imageName)
 		if err != nil {
 			panic(err)
 		}
@@ -54,6 +70,6 @@ func listen(topic string) {
 			Quality: 90,
 		}
 		jpeg.Encode(f, img, &opt)
-		log.Info("done")
+		log.Infof("Uploading...%s", imageName)
 	})
 }
